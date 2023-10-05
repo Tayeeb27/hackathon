3f8951f2cdbfe73dc1842e07e79c27803a579369
“AndrewAmir003” <andrewamir003@gmail.com>
2023-10-05 16:56:44 +0100

    models done almost, controllers almost done and routers almost done

--- Backend/controllers/diary.js
+++ Backend/controllers/diary.js
@@ -2,17 +2,38 @@ const Diary = require('../models/Diary');
 
 async function index(req, res) {
     try {
-        const countries = await Diary.getAll();
-        res.status(200).json(countries);
+        const diary = await Diary.getAll();
+        res.status(200).json(diary);
     } catch(err) {
         res.status(500).json({error: err.message})
     }
 }
 
-async function show (req, res) {
+async function showID (req, res) {
     try {
-        let name = req.params.name.toLowerCase();
-        const diary = await Diary.getOneByCountryName(name);
+        let id = parseInt(req.params.id);
+        const diary = await Diary.getOneByID(id);
+        res.status(200).json(diary)
+    } catch(err) {
+        res.status(404).json({error: err.message})
+    }
+}
+
+async function showCategory (req, res) {
+    try {
+        let category = req.params.category.toLowerCase();
+        const diary = await Diary.getOneByCategory(category);
+        res.status(200).json(diary)
+    } catch(err) {
+        res.status(404).json({error: err.message})
+    }
+}
+
+async function showDate (req, res) {
+    try {
+        let date = req.params.date;
+        console.log(date)
+        const diary = await Diary.getOneByDate(date);
         res.status(200).json(diary)
     } catch(err) {
         res.status(404).json({error: err.message})
@@ -22,8 +43,8 @@ async function show (req, res) {
 async function create (req, res) {
     try {
         const data = req.body;
-        const newCountry = await Diary.create(data);
-        res.status(201).json(newCountry);
+        const newDiary = await Diary.create(data);
+        res.status(201).json(newDiary);
     } catch(err) {
         res.status(400).json({error: err.message});
     }
@@ -43,13 +64,13 @@ async function update (req, res) {
 
 async function destroy (req, res) {
     try {
-        const name = req.params.name.toLowerCase();
-        const diary = await Diary.getOneByCountryName(name);
+        let id = parseInt(req.params.id);
+        const diary = await Diary.getOneByID(id);
         const result = await diary.destroy();
-        res.status(204).end();
+        res.status(204).json(result);
     } catch (err) {
         res.status(404).json({error: err.message})
     }
 };
 
-module.exports = { index, show, create, update, destroy }
+module.exports = { index, showID, showDate, showCategory, create, update, destroy }
--- Backend/routers/diary.js
+++ Backend/routers/diary.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
-const diaryController = require('../controllers/countries')
+const diaryController = require('../controllers/diary')
 
 const diaryRouter = Router();
 
 diaryRouter.get("/", diaryController.index);
-diaryRouter.get("/:name", diaryController.show);
+diaryRouter.get("/:id", diaryController.showID);
+diaryRouter.get("/date/:date", diaryController.showDate);
+diaryRouter.get("/category/:category", diaryController.showCategory);
 diaryRouter.post("/", diaryController.create);
-diaryRouter.patch("/:name", diaryController.update);
-diaryRouter.delete("/:name", diaryController.destroy);
+diaryRouter.patch("/:id", diaryController.update);
+diaryRouter.delete("/:id", diaryController.destroy);
 
 module.exports = diaryRouter;
